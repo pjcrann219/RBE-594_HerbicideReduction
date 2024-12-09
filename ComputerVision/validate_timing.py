487972ac0f45bc497ba5c031fc3cb511ba139d5a
Paul PC <pcrann@gmail.com>
2024-12-09 14:14:26 -0500

    update validate timing script

--- ComputerVision/validate_timing.py
+++ ComputerVision/validate_timing.py
@@ -45,14 +45,25 @@ def run_timing_eval(model, num_images=32, balance_ratio=0.50, isViT=False):
 
     return batch_times
 
-# device = torch.device("cuda" if torch.cuda.is_available() else ('mps' if torch.backends.mps.is_available() else "cpu"))
-device = "cpu"
-model = load_model('models/CNN/2024-11-19 10:05:54/full_model.pth', device=device)
+device = torch.device("cuda" if torch.cuda.is_available() else ('mps' if torch.backends.mps.is_available() else "cpu"))
+# device = "cpu"
+# model = load_model('models/CNN/2024-11-19 10:05:54/full_model.pth', device=device)
+# model = load_model('models/RESNET/2024-11-19 13:13:15/full_model.pth', device=device)
+model = load_model('models/ViT/2024-11-30 11:51:01/full_model.pth', device=device)
 
-times = run_timing_eval(model, num_images=320, balance_ratio=0.50, isViT=False)
+times = run_timing_eval(model, num_images=320, balance_ratio=0.50, isViT=True)
 avg_time_ms = sum(times) / len(times) / 32 * 1000
 print(f"Average time per image: {avg_time_ms:0.2f}ms")
 
 # Time Trials:
+# CNN
 # GPU: 1.55ms per image
 # CPU: 39.46ms per image
+
+# ResNet
+# GPU: 4.91ms per image
+# CPU: X ms per image
+
+# ViT
+# GPU: 13.16 ms per image
+# CPU: X ms per image
